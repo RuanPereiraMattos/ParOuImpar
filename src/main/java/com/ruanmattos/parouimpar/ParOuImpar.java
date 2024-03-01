55732649e6fd808e4cc864ad33b5c570a6813af0
Ruan <ruandupg@gmail.com>
2024-03-01 20:29:10 -0300

    Primeira versão do programa que calcula se o programa é par ou ímpar de acordo com o número digitado pelo cliente

---
+++ src/main/java/com/ruanmattos/parouimpar/ParOuImpar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ruanmattos.parouimpar;
+
+/**
+ *
+ * @author aluno
+ */
+public class ParOuImpar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

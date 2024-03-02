680b8e3d017ee6b966b84f84ec5a5be5e83e5ab2
Ruan <ruandupg@gmail.com>
2024-03-01 21:13:10 -0300

    Primeira versão do programa com mensagem de boas vindas e com a implementação completa para que o usuário digite o valor e o programa verifique através da operação de 'sobra' em relação ao número 0 (zero)

--- src/main/java/com/ruanmattos/parouimpar/ParOuImpar.java
+++ src/main/java/com/ruanmattos/parouimpar/ParOuImpar.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.ruanmattos.parouimpar;
+
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +11,19 @@ package com.ruanmattos.parouimpar;
  */
 public class ParOuImpar {
 
+    /**
+     * Programa que verifica se o número digitado pelo usuário é par ou impar
+     * @param args 
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bom vindo(a) ao programa que irá lhe 'dizer' se o número digitado por vossa pessoa é positivo ou negativo");
+        System.out.print("Digite o número que deseja verificar: ");
+        Scanner s = new Scanner(System.in);
+        double verificar = s.nextDouble();
+        if ((verificar % 2) == 0) {
+            System.out.println("O número digitado é par");
+        }else{
+            System.out.println("O número digitado é impar");
+        }
     }
 }
